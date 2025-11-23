c1a0c5a83128c6fe77837adcba8b4191943d9e1c
atrevetealonuevo <claudioandres.urra@gmail.com>
2025-11-23 01:47:53 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('generar-reporte').addEventListener('click', function() {
+    alert('Reporte generado!');
+});
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Formulario enviado. Gracias por contactarnos!');
+});
